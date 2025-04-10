f15af3126f21457bd3668f1fe45c901e3c73ae87
Juan_Rodriguez05 <modzjuanes94@gmail.com>
2025-04-10 00:03:36 -0500

    Pay Mode Presenter

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fee6cb9c4797252a8840ff072a0f11f4d4f74dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346d9f17cb3b1e52d4b7804fc9628f5f175bb1fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
