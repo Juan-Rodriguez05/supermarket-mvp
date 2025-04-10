cb25513cbfc55be8cc163995f904c84b7bba7f8b
Juan_Rodriguez05 <modzjuanes94@gmail.com>
2025-04-09 22:15:47 -0500

    Create Interfaz PayMode

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64da8b193f964f372cd371b406a2486d45531810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6fd9bf24c677474045349a018f46c24023871f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
