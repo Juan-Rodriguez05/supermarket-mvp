0c22bd849d6d73bb18f8cf12498e464ac3d700cc
Juan_Rodriguez05 <modzjuanes94@gmail.com>
2025-04-11 23:45:31 -0500

    Change Products Table SQL

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86300886925fbfcde581ad2624dbf9948b52cdad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3365efe627186883eaa220f951ffa77ea2f8b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
